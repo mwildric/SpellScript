992b6b0d49f0617c0361e9f41c0715680f33ba18
Matthew Wildrick <mwildric@gmail.com>
2019-08-07 23:25:09 -0400

    Progress with yaml generation

--- GetSpells.py
+++ GetSpells.py
@@ -1,8 +1,11 @@
 from lxml import html
 import requests
 import re
+import yaml
 
-page = requests.get('https://2e.aonprd.com/Spells.aspx?ID=4')
+
+
+page = requests.get('https://2e.aonprd.com/Spells.aspx?ID=500')
 tree = html.fromstring(page.content)
 
 name = tree.xpath('//h1[@class="title"]/text()')
@@ -13,21 +16,42 @@ traditions = tree.xpath('//span/a[contains(@href,"Tradition")]/text()')
 
 actions = tree.xpath('//b[starts-with(text(),\'Cast\')]/following-sibling::img/following-sibling::img/following-sibling::text()')
 
+rarity = tree.xpath('//b[starts-with(text(),\'Rarity\')]/following-sibling::text()[1]')
+area = tree.xpath('//b[starts-with(text(),\'Area\')]/following-sibling::text()[1]')
 spellrange = tree.xpath('//b[starts-with(text(),\'Range\')]/following-sibling::text()[1]')
 duration = tree.xpath('//b[starts-with(text(),\'Duration\')]/following-sibling::text()[1]')
 targets = tree.xpath('//b[starts-with(text(),\'Targets\')]/following-sibling::text()[1]')
 throw = tree.xpath('//b[starts-with(text(),\'Saving Throw\')]/following-sibling::text()[1]')
 
+description = re.findall( r'DetailedOutput.*?<hr />(.*?)<hr|<span/>', page.content.decode('utf-8'),  re.DOTALL)
 
-text = re.findall( r'((?<=<hr>)|(?<=<hr />)).*?(?=(<hr ?/?>|</span>))' , page.content.decode('utf-8'), re.IGNORECASE)
-print(text)
+heightened = re.findall( r'(?<=Heightened )\((.*?)\)</b> (.*?)(<b>|</span>)' , page.content.decode('utf-8'), re.IGNORECASE)
 
+f = open("all.yaml", "w")
+yaml.dump(
+    {
+        'name': name,
+        'traits': traits,
+        'traditions': traditions,
+        'level': level,
+        'type': "",
+        'rarity': "",
+        'casting': "",
+        'area':area,
+        'range':spellrange,
+        'target': targets,
+        'saving throw': throw,
+        'duration': duration,
+        'description':{
+            'main':description
+        },
+        'heightened': heightened     
 
-heighten = re.findall( r'(?<=Heightened )\((.*?)\)</b> (.*?)(<b>|</span>)' , page.content.decode('utf-8'), re.IGNORECASE)
-print(heighten)
+    }, f
+    
+    )
+f.close
 
-#heightened = tree.xpath('//b[starts-with(text(),\'Heightened\')]/following-sibling::text()')
-
-#(?<=Heightened )\((.*?)\)</b> (.*?)(<b?|$)
 
 #(?<=<hr>).*(?=(<hr>|$))
+#'((?<=<hr>)|(?<=<hr />)).*?(?=(<hr ?/?>|</span>))' 
